08224d1e7ca61bab31f7958cfe8be19c63aaaf91
xyberista <50119873+Xyberista@users.noreply.github.com>
2022-08-18 18:55:25 -0500

    minimal working yew page

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html, css, rs}", "./static/index.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
